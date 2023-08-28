ae2451d223f26189606a47620ce7f8d42bc0e605
Ray.ly <120208761+Ray-ly-04@users.noreply.github.com>
2023-08-28 15:09:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests 
+ import requests 
+  
+ elkuser = "user" 
+ elkpass = "pass" 
+  
+ print(""" 
+                                                                               ffffffffffffffff 
+                                                                              f::::::::::::::::f 
+                                                                             f::::::::::::::::::f 
+                                                                             f::::::fffffff:::::f 
+     ssssssssss      mmmmmmm    mmmmmmm      ooooooooooo      ooooooooooo    f:::::f       ffffff 
+   ss::::::::::s   mm:::::::m  m:::::::mm  oo:::::::::::oo  oo:::::::::::oo  f:::::f 
+ ss:::::::::::::s m::::::::::mm::::::::::mo:::::::::::::::oo:::::::::::::::of:::::::ffffff 
+ s::::::ssss:::::sm::::::::::::::::::::::mo:::::ooooo:::::oo:::::ooooo:::::of::::::::::::f 
+  s:::::s  ssssss m:::::mmm::::::mmm:::::mo::::o     o::::oo::::o     o::::of::::::::::::f 
+    s::::::s      m::::m   m::::m   m::::mo::::o     o::::oo::::o     o::::of:::::::ffffff 
+       s::::::s   m::::m   m::::m   m::::mo::::o     o::::oo::::o     o::::o f:::::f 
+ ssssss   s:::::s m::::m   m::::m   m::::mo::::o     o::::oo::::o     o::::o f:::::f 
+ s:::::ssss::::::sm::::m   m::::m   m::::mo:::::ooooo:::::oo:::::ooooo:::::of:::::::f 
+ s::::::::::::::s m::::m   m::::m   m::::mo:::::::::::::::oo:::::::::::::::of:::::::f 
+  s:::::::::::ss  m::::m   m::::m   m::::m oo:::::::::::oo  oo:::::::::::oo f:::::::f 
+   sssssssssss    mmmmmm   mmmmmm   mmmmmm   ooooooooooo      ooooooooooo   fffffffff 
+  
+  
+   ray.xD
+  
+ """),
+  
+  
+ print "To:", 
+ recipient = raw_input() 
+ print "From:", 
+ sender = raw_input() 
+ print "Message:", 
+ messagetext = raw_input() 
+  
+ url = "https://api.46elks.com/a1/SMS" 
+ r = requests.post(url, data={'to': recipient,'from': sender,'message': messagetext}, auth=(elkuser, elkpass)) 
+ print r.json()￼Enter
